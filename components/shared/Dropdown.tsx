87e25ba40cdb3000fa718af54800970fe5f5d65b
PhDBui <congdoan6997@gmail.com>
2024-01-03 17:37:34 +0300

    future event page

---
+++ components/shared/Dropdown.tsx
@@ -0,0 +1,94 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { ICategory } from "@/lib/database/models/category.model";
+import { useEffect, useState } from "react";
+import { Input } from "../ui/input";
+import {
+  createCategory,
+  getAllCategories,
+} from "@/lib/actions/category.actions";
+
+interface IDropdownProps {
+  onChangeHandler?: () => void;
+  value?: string;
+}
+
+const Dropdown = ({ onChangeHandler, value }: IDropdownProps) => {
+  const [categories, setCategories] = useState<ICategory[]>([]);
+  const [newCategory, setNewCategory] = useState<string>("");
+
+  const handleAddCategory = async () => {
+    const newCat = await createCategory({ categoryName: newCategory.trim() });
+    setCategories([...categories, newCat]);
+  };
+  useEffect(() => {
+    const getCategories = async () => {
+      const cats = await getAllCategories();
+      cats && setCategories(cats as ICategory[]);
+    };
+    getCategories();
+  }, []);
+
+  return (
+    <Select onValueChange={onChangeHandler} defaultValue={value}>
+      <SelectTrigger className="select-field">
+        <SelectValue placeholder="Category" />
+      </SelectTrigger>
+      <SelectContent>
+        {categories.length > 0 &&
+          categories.map((category) => (
+            <SelectItem
+              key={category._id}
+              value={category._id}
+              className="select-item p-regular-14"
+            >
+              {category.name}
+            </SelectItem>
+          ))}
+
+        <AlertDialog>
+          <AlertDialogTrigger className="p-medium-14 flex w-full rounded-sm py-3 pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500">
+            Add new category
+          </AlertDialogTrigger>
+          <AlertDialogContent className="bg-white">
+            <AlertDialogHeader>
+              <AlertDialogTitle>New Category</AlertDialogTitle>
+              <AlertDialogDescription>
+                <Input
+                  type="text"
+                  placeholder="Category name"
+                  className="input-field mt-3"
+                  onChange={(e) => setNewCategory(e.target.value)}
+                />
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction onClick={handleAddCategory}>
+                Add
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default Dropdown;
